052cae7a1ef4267cfa32d8e78dd4f9e9a62d965c
julianostroschon <julianostroschon@gmail.com>
2024-05-23 17:20:50 -0300

    🚑️ Add binding de porta

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,10 +4,9 @@ import schema from './schema/generated';
 
 
 export function buildServer (port: number): void {
-
   const yoga = createYoga(schema)
   const server = createServer(yoga)
-  server.listen(3000, () => {
+  server.listen(port, () => {
     console.log(`Server is running on http://0.0.0.0:${port}/graphql`)
   })
 }
