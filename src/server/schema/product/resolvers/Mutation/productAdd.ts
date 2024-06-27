8bc2b1df14e95b0b595e004cd91f83a8882905d0
julianostroschon <julianostroschon@gmail.com>
2024-06-27 20:55:41 -0300

    ✨ ProductAdd

---
+++ src/server/schema/product/resolvers/Mutation/productAdd.ts
@@ -0,0 +1,9 @@
+import type { MutationResolvers } from "./../../../generated/types.generated.d";
+
+export const productAdd: NonNullable<MutationResolvers["productAdd"]> = async (
+  _parent,
+  { dados },
+  { database },
+) => {
+  return database("products").insert(dados).returning("*");
+};
