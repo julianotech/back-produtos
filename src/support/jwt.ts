a904d68115cee3c9fedb0f21176ab858c6300ab7
julianostroschon <julianostroschon@gmail.com>
2024-10-14 20:09:47 -0300

    ✨ Implementa assinatura de token

---
+++ src/support/jwt.ts
@@ -0,0 +1,10 @@
+import { sign } from 'jsonwebtoken';
+
+import { env } from '@/infra/config';
+
+
+export function generateToken(payload: Record<string, any>): string {
+  return sign(payload, env.jwtSecret, {
+    expiresIn: '2h'
+  });
+}
