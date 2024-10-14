061379bf6474fda76e4c78ab49bc624826ce03ca
julianostroschon <julianostroschon@gmail.com>
2024-10-14 19:43:17 -0300

    ✏️ Corrige tipo do schema

--- src/server/generated/index.ts
+++ src/server/generated/index.ts
@@ -1,9 +1,10 @@
 import { typeDefs } from './typeDefs.generated';
 import { resolvers } from './resolvers.generated';
-import { createSchema } from 'graphql-yoga';
+import { createSchema, YogaServerOptions } from 'graphql-yoga';
 import { constructCtx } from '../context';
+import { Context } from '../contracts';
 
 export default {
   schema: createSchema({ resolvers, typeDefs }),
   context: constructCtx(),
-};
+} as YogaServerOptions<Context, {}>;
