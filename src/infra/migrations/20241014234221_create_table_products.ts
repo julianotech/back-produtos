9606a85838d4e78f0a4d7ca35cb42eeefa125bc8
julianostroschon <julianostroschon@gmail.com>
2024-10-14 20:48:32 -0300

    📝 Cria migrations products

---
+++ src/infra/migrations/20241014234221_create_table_products.ts
@@ -0,0 +1,16 @@
+import type { Knex } from 'knex';
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('products', (table) => {
+    table.increments('id');
+    table.string('name');
+    table.integer('price').defaultTo(1);
+    table.integer('stock').defaultTo(0);
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('products');
+}
